c7d5f6356a160b1a41f12f319535890de08ef4e9
KainAber <dariogino@gmail.com>
2026-06-03 15:39:13 +0200

    Package refactor

--- main.py
+++ main.py
@@ -15,10 +15,14 @@ from src.latex_cv import clean_unused_tags, fill_template
 
 
 def run_engine(input_folder_path: Path, output_folder_path: Path) -> None:
-
     # Get latest config path
     cfg_path = get_latest_file(input_folder_path, ext=".yml")
 
+    # Run from path
+    run_latex_cv_engine_from_cfg_path(cfg_path, output_folder_path)
+
+
+def run_latex_cv_engine_from_cfg_path(cfg_path: Path, output_folder_path: Path) -> None:
     # Read config
     with open(cfg_path, "r") as f:
         cfg = yaml.safe_load(f)
@@ -27,7 +31,7 @@ def run_engine(input_folder_path: Path, output_folder_path: Path) -> None:
     create_img_folder(output_folder_path)
 
     # Update photo
-    cfg = update_and_save_photo(cfg, input_folder_path, output_folder_path)
+    cfg = update_and_save_photo(cfg, cfg_path.parent, output_folder_path)
 
     # Extract icons color
     icons_color = str(cfg["colors"]["accent"])
